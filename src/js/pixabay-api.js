5b2ce2c51b2f280d25ca1a0e1af75854587bf0f0
Nevlen3 <nevlen3@gmail.com>
2025-06-28 11:50:24 +0200

    ver 2

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '50910167-da0c49dcf69b8279e7055dacc';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
+}
---
+++ src/main.js
@@ -0,0 +1,42 @@
+import './css/styles.css';
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+const params = new URLSearchParams(window.location.search);
+const initialQuery = params.get('search-text');
+if (initialQuery) {
+  form.elements['search-text'].value = initialQuery;
+  form.dispatchEvent(new Event('submit'));
+}
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const query = e.target.elements['search-text'].value.trim();
+
+  if (!query) {
+    iziToast.warning({ message: 'Будь ласка, введіть слово для пошуку!', position: 'topRight' });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+    const images = data.hits;
+
+    if (images.length === 0) {
+      iziToast.info({ message: 'Sorry, there are no images matching your search query. Please try again!', position: 'topRight' });
+    } else {
+      createGallery(images);
+    }
+  } catch (err) {
+    iziToast.error({ message: 'Сталася помилка. Спробуйте ще раз.', position: 'topRight' });
+  } finally {
+    hideLoader();
+  } 
+});
